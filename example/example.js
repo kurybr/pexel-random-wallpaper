af37b3d513da0a783e1bc034b8806ae1d6cee4fe
Jorge Rafael <hello@foca.dev>
2020-09-27 10:39:06 -0300

    🎨 Example how to use

---
+++ example/example.js
@@ -0,0 +1,22 @@
+const Path = require('path');
+const fs = require('fs');
+const PexelsRandom = require('../lib/pexel-random-wallpaper');
+
+
+(async () => { 
+
+    const directory = Path.join(process.env.HOME, '/Pictures/wallpapers-pexels')
+
+    // If don't exist the directory, make it.
+    if(!fs.existsSync(directory)){ fs.mkdirSync(directory) }
+
+    const client = new PexelsRandom({ path: directory, api: ''});
+    const image = await client.handleSearch('lavanda, africa, nature');
+
+    client.SetWallpaper(image)
+   
+
+})();
+
+
+
